bc8a86b0ac75c63319ac9a685ea0b9e17d73c6dd
MeiKurasaka <mkurasaka7522@gmail.com>
2021-04-26 14:41:07 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,41 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    post = Post.new(post_params)
+    post.save
+    redirect_to posts_url, notice: 'Created'
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to posts_url, notice: 'Updated'
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to posts_url, notice: 'Destroyed'
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
